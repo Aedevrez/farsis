ccaec92768a837f9cdb0247f53a9aa390687d890
Adem Efe Devrez <adevrez77@gmail.com>
2024-12-04 23:30:58 +0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace farsis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
